f72328e565c84058cdb26c17da44341f0a06b7bc
Yu Yuan <yuzyuan93@gmail.com>
2015-12-03 19:26:07 -0500

    test php v3

--- connection.php
+++ connection.php
@@ -7,7 +7,7 @@
         if( $g_link )
             return $g_link;
         $g_link = mysql_connect( 'overtherainbow.cmd36kni6cfz.us-west-2.rds.amazonaws.com:3306', 'RainbowOverlord', '2007)nyxIr1s') or die('Could not connect to server.' );
-        mysql_select_db('database_name', $g_link) or die('Could not select database.');
+        mysql_select_db('OverTheRainbow', $g_link) or die('Could not select database.');
         return $g_link;
     }
     
